f70d0f401f58a9a8d6b3b265cbf625960d70e53f
Garsiet <garsiet1@gmail.com>
2016-11-14 14:54:17 +0700

    calculating mean score

--- app/models/teacher.py
+++ app/models/teacher.py
@@ -23,8 +23,8 @@ class Subject:
 
         #connect = sqlite3.connect('Data.db')
         #c = connect.cursor()
-        #all_score =
-        #all_student =
+        #all_score = c.execute("Select student_ID =")
+        #all_student = Stusent_ID
         #mean = all_score/all_student
 
     def Get_grade_mark_by_subject(self):
