b4b27a849940b50597a722bf651475cedd127c0d
Kitti <bosskitti7983@outlook.com>
2016-11-15 12:44:00 +0700

    add full mark in score

--- app/models/work.py
+++ app/models/work.py
@@ -15,16 +15,15 @@ class Work:
         self.Date_check = ""
         self.submit_work = self.Get_submit_work()
         self.Deadline = self.Get_deadline()
-        self.Fullmark = self.Fullmark()
+        self.Fullmark = self.Get_fullmark()
         self.grad = self.Get_grad()
 
     def Get_deadline(self):
         connect = sqlite3.connect("Data.db")
         c = connect.cursor()
         # get deadline
-        member_list = c.execute("SELECT Deadlines from work WHERE WorkID = "
-                                + str(self.WorkID) + " AND Year = " + str(self.year) + " AND Subject_ID = "
-                                + str(self.Subject_Id))
+        member_list = c.execute("SELECT Deadlines from work WHERE WorkID = ?  AND Year = ?  AND Subject_ID = ? ",
+                                (str(self.Work_Id),str(self.year),str(self.Subject_ID)))
         deadline = member_list.fetchone()
         # close connection
         c.close()
@@ -35,9 +34,8 @@ class Work:
         connect = sqlite3.connect("Data.db")
         c = connect.cursor()
         # get fullmark
-        fullmark = c.execute("SELECT FullMark from work WHERE Subject_ID = "
-                             + str(self.Subject_ID) + " Year = " + str(self.year) +
-                             " WorkID = " + str(self.Work_Id))
+        fullmark = c.execute("SELECT FullMark from work WHERE Subject_ID = ? AND Year = ? AND WorkID = ?"
+                             ,(str(self.Subject_ID),str(self.year),str(self.Work_Id)))
         fullmark = fullmark.fetchone()
         # close connection
         c.close()
@@ -58,34 +56,34 @@ class Work:
         return str(status[0])
 
     def Get_submit_work(self):
-        connect = sqlite3.connect("Data.db")
-        c = connect.cursor()
-        # get submit work
-        # failed
-        submit_work = c.execute("SELECT .. from SubmitWork WHERE Subject_ID = "
-                                + str(self.Subject_ID) + "AND Year = " + str(self.year) +
-                                "AND WorkID = " + str(self.Work_Id))
-        Submit_Work = {}
-        for x in submit_work.fetchall():
-            Submit_Work[str(x[3])] = submitWork(self.Work_Id, self.Subject_ID, self.year, str(x[3]))
-        # close connection
-        c.close()
-        # return status [list]
-        return submit_work
+        # connect = sqlite3.connect("Data.db")
+        # c = connect.cursor()
+        # # get submit work
+        # # failed
+        # submit_work = c.execute("SELECT .. from SubmitWork WHERE Subject_ID = "
+        #                         + str(self.Subject_ID) + "AND Year = " + str(self.year) +
+        #                         "AND WorkID = " + str(self.Work_Id))
+        # Submit_Work = {}
+        # for x in submit_work.fetchall():
+        #     Submit_Work[str(x[3])] = submitWork(self.Work_Id, self.Subject_ID, self.year, str(x[3]))
+        # # close connection
+        # c.close()
+        # # return status [list]
+        # return submit_work
+        pass
 
     def Get_grad(self):
-        connect = sqlite3.connect("Data.db")
-        c = connect.cursor()
-        # get grad
-        grad = c.execute("SELECT Garding from work where Subject_ID = "
-                            + str(self.Subject_ID) + " Year = " + str(self.year) +
-                            " WorkID = " + str(self.Work_Id))
-        grad = grad.fetchone()
-        # close connection
-        c.close()
-        # return grad 'str'
-        return str(grad[0])
-
+        # connect = sqlite3.connect("Data.db")
+        # c = connect.cursor()
+        # # get grad
+        # grad = c.execute("SELECT Garding from work WHERE Subject_ID = ? AND Year = ? AND WorkID = ?"
+        #                  ,(str(self.Subject_ID),str(self.year),str(self.Work_Id)))
+        # grad = grad.fetchone()
+        # # close connection
+        # c.close()
+        # # return grad 'str'
+        # return str(grad[0])
+        pass
     def Mark_decending(self):
         # get mark as key then use sortbykey u canuse map lambda whatever
         pass
