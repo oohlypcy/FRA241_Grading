e2f924f9760667be92fecf80234432a0185d03d4
Cha245689 <chaiya45689@gmail.com>
2016-11-17 17:13:51 +0700

    Merge branch 'master' of https://github.com/cha45689/FRA241_Grading
    # Conflicts:
    #	app/score/views.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,14 @@ from login.views import Login
 from homepage.views import Homepage
 from classpage.views import classpage
 from score.views import score
+from addpage.views import Addpage
 #declare flask app
 Grading = Flask(__name__)
 #register blueprint
 Grading.register_blueprint(Login)
 Grading.register_blueprint(Homepage)
 Grading.register_blueprint(classpage)
-Grading.register_blueprint(score)
-
+Grading.register_blueprint(Addpage)
 #default route
 @Grading.route('/')
 def default():
