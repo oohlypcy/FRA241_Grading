ed18c0259c60bcf41d3957c82b7bc4d4c0cfc121
Cha245689 <chaiya45689@gmail.com>
2016-11-19 02:10:41 +0700

    Merge branch 'master' of https://github.com/cha45689/FRA241_Grading
    # Conflicts:
    #	app/score/views.py

--- app/addpage/views.py
+++ app/addpage/views.py
@@ -5,30 +5,49 @@ from app.models.subject import Subject
 from app.models.submitWork import submitWork
 from app.models.work import Work
 import datetime
+
 # create Blueprint class with name importname Blueprintfolders
-Addpage= Blueprint('Addpage', __name__, url_prefix="/<url_user_id>", template_folder='', static_folder='')
+Addpage = Blueprint('Addpage', __name__, url_prefix="/<url_user_id>", template_folder='', static_folder='')
+
 
 @Addpage.url_value_preprocessor
-def addpage(endpoint,url_user_id):
-    g.user=User(url_user_id['url_user_id'])
+def addpage(endpoint, url_user_id):
+    g.user = User(url_user_id['url_user_id'])
+
 
 @Addpage.route('/<url_Subject_id>/<url_Year>/add_work')
-def add_assignment(url_user_id,url_Subject_id,url_Year):
+def add_assignment(url_user_id, url_Subject_id, url_Year):
     return 'boo'
+
 
 @Addpage.route('/add_subject')
 def add_subject(url_user_id):
     return render_template('teacher_add_subject.html')
 
+
 @Addpage.route('/<url_Subject_id>/<url_Year>/add_student')
-def add_student(url_user_id,url_Subject_id,url_Year):
+def add_student(url_user_id, url_Subject_id, url_Year):
+    conn = sqlite3.connect('Data.db')
+    currentAcademicYear = datetime.date.today()
+    if currentAcademicYear.month <= 4:
+        currentAcademicYear = currentAcademicYear.year + 542
+    else:
+        currentAcademicYear = currentAcademicYear.year + 543
+    g.YEAR = [x + currentAcademicYear for x in range(0, 3)]
+    print g.YEAR
+    currentAcademicYear = str(int(str(currentAcademicYear)[2:4]))
+    c = conn.cursor()
+    subject_list = c.execute("SELECT Subject_ID from subject WHERE  Year =  ? ", (currentAcademicYear,))
+    g.subject_list = subject_list.fetchall()
+    c.close()
+    print g.subject_list
     return render_template('teacher_add_TA.html')
 
+
 @Addpage.route('/<url_Subject_id>/<url_Year>/<work_id>/manage_group')
-def manage_group(url_user_id,url_Subject_id,url_Year,work_id):
+def manage_group(url_user_id, url_Subject_id, url_Year, work_id):
     g.user = User(url_user_id)
-    if g.user.Profile['Role']=='student':
+    if g.user.Profile['Role'] == 'student':
         return render_template('student_grouping.html')
     else:
         return render_template('grouping.html')
-
