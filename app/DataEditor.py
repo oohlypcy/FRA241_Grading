0fa7dbde789e5098a132cea437112dbb44f9dbfb
zefirosenix <zefirosfenix@gmail.com>
2016-11-20 16:01:58 +0700

    Merge branch 'master' into HEAD
    # Conflicts:
    #	app/homepage/HTML_assignment.html
    #	app/models/subject.py
    
    me try committing again my fellows <3

--- app/DataEditor.py
+++ app/DataEditor.py
@@ -1,14 +1,15 @@
 import sqlite3
 
+
 class Data:
-    def __init__(self):    
+    def __init__(self):
         """ for make table and input manual data """
-        conn = sqlite3.connect('Data.db') #connect db name Data.db
+        conn = sqlite3.connect('Data.db')  # connect db name Data.db
         print "Connect : pass"
-        c= conn.cursor()    #prepare from modify db
-    
+        c = conn.cursor()  # prepare from modify db
+
         # create table name user
-        try :
+        try:
             c.executescript("""
             CREATE TABLE `User` (
               `ID` bigint(20) NOT NULL,
@@ -25,10 +26,9 @@ class Data:
             )""")
         except Exception:
             print "Table User has created"
-    
-    
+
         # create table name Enrol
-        try :
+        try:
             c.executescript("""CREATE TABLE `Enrol` (
               `ID` bigint(11) NOT NULL,
               `Subject_ID` varchar(45) NOT NULL,
@@ -36,8 +36,7 @@ class Data:
             )""")
         except Exception:
             print "Table Enrol has created"
-    
-    
+
         # create table name Groups
         try:
             c.executescript("""
@@ -49,10 +48,9 @@ class Data:
             )""")
         except Exception:
             print "Table Groups has created"
-    
-    
+
         # create table name media
-        try :
+        try:
             c.executescript("""
             CREATE TABLE `media` (
               `Subject_ID` varchar(45) NOT NULL,
@@ -64,9 +62,8 @@ class Data:
             )""")
         except Exception:
             print "Table media has created"
-    
-    
-        #create table name subject
+
+        # create table name subject
         try:
             c.executescript("""
             CREATE TABLE `subject` (
@@ -78,9 +75,8 @@ class Data:
             )""")
         except Exception:
             print "Table Subject has created"
-    
-    
-        #create table name Submitwork
+
+        # create table name Submitwork
         try:
             c.executescript("""
             CREATE TABLE `SubmitWork` (
@@ -94,9 +90,9 @@ class Data:
             )""")
         except Exception:
             print "Table Submit work has created"
-    
-        #create table name work
-        try :
+
+        # create table name work
+        try:
             c.executescript("""
             CREATE TABLE `work` (
               `Subject_ID` varchar(45) NOT NULL,
@@ -113,65 +109,73 @@ class Data:
             print "Table Work has created"
         c.close()
 
-
     # insert data in User table
-    def UserInsert(self,ID, Password, Title, Name, Surname, E_mail, Role, Faculty, Major, Enrol_Year, Picture): #ID and Password aren't NULL
+    def UserInsert(self, ID, Password, Title, Name, Surname, E_mail, Role, Faculty, Major, Enrol_Year,
+                   Picture):  # ID and Password aren't NULL
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
         c.execute("""INSERT INTO `User` (`ID`, `Password`, `Title`, `Name`, `Surname`, `E-mail`, `Role`, `Faculty`, `Major`, `Enrol-Year`, `Picture`) VALUES
-        (?,?,?,?,?,?,?,?,?,?,?);""",(ID, Password, Title, Name, Surname, E_mail, Role, Faculty, Major, Enrol_Year, Picture))
+        (?,?,?,?,?,?,?,?,?,?,?);""",
+                  (ID, Password, Title, Name, Surname, E_mail, Role, Faculty, Major, Enrol_Year, Picture))
         conn.commit()  # save data into db
         c.close()
+
     # insert data in Enrol table
-    def EnrolInsert(self,ID,Subject_ID,Subject_Year): #not NULL all
+    def EnrolInsert(self, ID, Subject_ID, Subject_Year):  # not NULL all
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
         c.execute("""INSERT INTO `Enrol` (`ID`, `Subject_ID`, `subject_Year`) VALUES
-        (?, ?, ?);""",(ID,Subject_ID,Subject_Year))
+        (?, ?, ?);""", (ID, Subject_ID, Subject_Year))
         conn.commit()  # save data into db
         c.close()
-    #insert data in Groups table
-    def GroupInsert(self,Subject_ID,Year,WorkID,ID): #not NULL all
+
+    # insert data in Groups table
+    def GroupInsert(self, Subject_ID, Year, WorkID, ID):  # not NULL all
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
         c.execute("""INSERT INTO `Groups` (`Subject_ID`, `Year`, `WorkID`, `ID`) VALUES
-         (?,?,?,?);""",(Subject_ID,Year,WorkID,ID))
+         (?,?,?,?);""", (Subject_ID, Year, WorkID, ID))
         conn.commit()  # save data into db
         c.close()
-    #insert data in media table
-    def mediaInsert(self,Subject_ID,Year,Description,FullMark,Grading): #Subject_ID and Year aren't NULL
+
+    # insert data in media table
+    def mediaInsert(self, Subject_ID, Year, Description, FullMark, Grading):  # Subject_ID and Year aren't NULL
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
         c.execute("""INSERT INTO `media` (`Subject_ID`,`Year`,`Description`,`FullMark`,`Grading`) VALUES
-         (?,?,?,?,?);""",(Subject_ID,Year,Description,FullMark,Grading))
+         (?,?,?,?,?);""", (Subject_ID, Year, Description, FullMark, Grading))
         conn.commit()  # save data into db
         c.close()
-    #insert data in subject table
-    def subjectInsert(self,Subject_ID, Year, Description, FullMark, Grading): #Description, FullMark and Grading default are NULL
+
+    # insert data in subject table
+    def subjectInsert(self, Subject_ID, Year, Description, FullMark,
+                      Grading):  # Description, FullMark and Grading default are NULL
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
         c.execute("""INSERT INTO `subject` (`Subject_ID`, `Year`, `Description`, `FullMark`, `Grading`) VALUES
-        (?,?,?,?,?);""",(Subject_ID,Year,Description,FullMark,Grading))
+        (?,?,?,?,?);""", (Subject_ID, Year, Description, FullMark, Grading))
         conn.commit()  # save data into db
         c.close()
-    #insert data in Submitwork table
-    def SubmitworkInsert(self,Subject_ID, Year, WorkID, ID, Address, Status, Mark): #Address, status and Mark default are NULL
+
+    # insert data in Submitwork table
+    def SubmitworkInsert(self, Subject_ID, Year, WorkID, ID, Address, Status,
+                         Mark):  # Address, status and Mark default are NULL
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
         c.execute("""INSERT INTO `Submitwork` (`Subject_ID`, `Year`, `WorkID`, `ID`, `Address`, `Status`, `Mark`) VALUES
-        (?,?,?,?,?,?,?);""",(Subject_ID, Year, WorkID, ID, Address, Status, Mark))
+        (?,?,?,?,?,?,?);""", (Subject_ID, Year, WorkID, ID, Address, Status, Mark))
         conn.commit()  # save data into db
         c.close()
-    #insert data in work table
-    def workInsert(self,Subject_ID, Year, WorkID, Deadlines, status, type, FullMark, Grading, lim_member): #Subject_ID, Year, WorkID aren't NULL
+
+    # insert data in work table
+    def workInsert(self, Subject_ID, Year, WorkID, Deadlines, status, type, FullMark, Grading,
+                   lim_member):  # Subject_ID, Year, WorkID aren't NULL
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
         c.execute("""INSERT INTO `work` (`Subject_ID`, `Year`, `WorkID`, `Deadlines`, `status`, `type`, `FullMark`, `Grading`, `lim_member`) VALUES
-        (?,?,?,?,?,?,?,?,?);""",(Subject_ID, Year, WorkID, Deadlines, status, type, FullMark, Grading, lim_member))
+        (?,?,?,?,?,?,?,?,?);""", (Subject_ID, Year, WorkID, Deadlines, status, type, FullMark, Grading, lim_member))
         conn.commit()  # save data into db
         c.close()
-
-
 
     # UserInsert(ID='58340500017',Password='Boomming1*',Title='Mr.',Name='Chaiyaporn',Surname='Boonyasathian',E_mail='chaiya45689@gmail.com',Role='student',Faculty='FIBO',Major='robotic and automation',Enrol_Year='58',Picture=None) #insert User
     # subjectInsert(Subject_ID='FRA222',Year='59',Description=None,FullMark=None,Grading=None) #insert subject
@@ -200,7 +204,7 @@ class Data:
 
     # c.execute("UPDATE User SET Picture ='Untitled.png' WHERE ID ='58340500005'")
 
-    def edit(self,code):
+    def edit(self, code):
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
         c.execute(str(code))
@@ -212,7 +216,8 @@ class Data:
         print("-----------User-----------")
         conn = sqlite3.connect("Data.db")
         c = conn.cursor()
-        cursor = c.execute("SELECT ID, Password, Title, Name, Surname, Role, Faculty, Major, Picture from User") #choose table for search data
+        cursor = c.execute(
+            "SELECT ID, Password, Title, Name, Surname, Role, Faculty, Major, Picture from User")  # choose table for search data
         for row in cursor:
             print "ID = ", row[0]
             print "Password = ", row[1]
@@ -221,8 +226,8 @@ class Data:
             print "Surname = ", row[4]
             print "Role = ", row[5]
             print "Faculty = ", row[6]
-            print "Major =",row[7]
-            print "Picture = ",row[8]
+            print "Major =", row[7]
+            print "Picture = ", row[8]
             print "***************"
 
         print("-----------Enrol-----------")
@@ -252,32 +257,32 @@ class Data:
         print("-----------subject-----------")
         cursor = c.execute("SELECT Subject_ID, Year, Description,FullMark, Grading from subject")
         for row in cursor:
-            print "Subject ID = ",row[0]
-            print "Year = ",row[1]
-            print "Description = ",row[2]
-            print "FullMark = ",row[3]
-            print "Grading = ",row[4]
+            print "Subject ID = ", row[0]
+            print "Year = ", row[1]
+            print "Description = ", row[2]
+            print "FullMark = ", row[3]
+            print "Grading = ", row[4]
             print "***************"
 
         print("-----------SubmitWork-----------")
         cursor = c.execute("SELECT Subject_ID, Year, WorkID, ID, Address, Status, Mark from SubmitWork")
         for row in cursor:
-            print "Subject ID = ",row[0]
-            print "Year = ",row[1]
-            print "WorkID = ",row[2]
-            print "ID = ",row[3]
-            print "Address = ",row[4]
-            print "Status = ",row[5]
-            print "Mark = ",row[6]
+            print "Subject ID = ", row[0]
+            print "Year = ", row[1]
+            print "WorkID = ", row[2]
+            print "ID = ", row[3]
+            print "Address = ", row[4]
+            print "Status = ", row[5]
+            print "Mark = ", row[6]
             print "***************"
 
         print("-----------work-----------")
         cursor = c.execute("SELECT Subject_ID, Year, WorkID, Deadlines, status, FullMark, lim_member from work")
         for row in cursor:
-            print "Subject ID = ",row[0]
-            print "Year = ",row[1]
-            print "WorkID = ",row[2]
-            print "Deadline = ",row[3]
+            print "Subject ID = ", row[0]
+            print "Year = ", row[1]
+            print "WorkID = ", row[2]
+            print "Deadline = ", row[3]
             print "status = ", row[4]
             print "FullMark = ", row[5]
             print "lim_member = ", row[6]
@@ -287,9 +292,9 @@ class Data:
         cursor = c.execute("SELECT * from Enrol")
         for row in cursor:
             print row
-            print "ID = ",row[0]
-            print "Subject ID = ",row[1]
-            print "Subject year = ",row[2]
+            print "ID = ", row[0]
+            print "Subject ID = ", row[1]
+            print "Subject year = ", row[2]
             print "Enrol_Type = ", row[3]
             print "SECTION = ", row[4]
         print "***************"
@@ -299,21 +304,35 @@ class Data:
         # conn.commit()
         c.close()
 
+
 # c.executescript("""DROP TABLE User""") #delete table
 # c.executescript("""DELETE FROM subject WHERE Subject_ID = 'FRA142'""") #delete data in table subject
 # c.executescript("""DELETE FROM work WHERE Subject_ID ='FRA222'""") #delete work
-#c.execute("UPDATE SubmitWork SET Status = ? WHERE Status = ?",('sent',' not send'))
-#c.execute("ALTER TABLE enrol ADD  COLUMN Enrol_Type varchar(45) DEFAULT NULL")
-#c.execute("ALTER TABLE enrol ADD  COLUMN SECTION varchar(45) DEFAULT NULL")
-#c.execute("UPDATE enrol SET Enrol_Type = ?,SECTION = ? WHERE ID = ?",('student','A','58340500017'))
-#c.execute("UPDATE enrol SET Enrol_Type = ?,SECTION = ? WHERE ID = ?",('student','A','58340500005'))
-#c.execute("UPDATE enrol SET Enrol_Type = ? WHERE ID = ?",('teacher','58340500000'))
-conn = sqlite3.connect('Data.db')
-c = conn.cursor()
-c.execute("UPDATE work SET Status = 'Closed' ")
-c.execute("UPDATE work SET Status = 'Closed' ")
-conn.commit()
-c.close
+# c.execute("UPDATE SubmitWork SET Status = ? WHERE Status = ?",('sent',' not send'))
+# c.execute("ALTER TABLE enrol ADD  COLUMN Enrol_Type varchar(45) DEFAULT NULL")
+# c.execute("ALTER TABLE enrol ADD  COLUMN SECTION varchar(45) DEFAULT NULL")
+# c.execute("UPDATE enrol SET Enrol_Type = ?,SECTION = ? WHERE ID = ?",('student','A','58340500017'))
+# c.execute("UPDATE enrol SET Enrol_Type = ?,SECTION = ? WHERE ID = ?",('student','A','58340500005'))
+# c.execute("UPDATE enrol SET Enrol_Type = ? WHERE ID = ?",('teacher','58340500000'))
+# conn = sqlite3.connect('Data.db')
+# c = conn.cursor()
+# c.execute("UPDATE work SET Status = 'Closed' ")
+# c.execute("UPDATE work SET Status = 'Closed' ")
+# conn.commit()
+# c.close
+s =sqlite3.connect('Data.db')
+h=s.cursor()
+c=h.execute("SELECT * from User WHERE ID = 58340500017")
+# print c.fetchall()
+print c.fetchone()
+h.close()
+ham = s.cursor()
+ham.execute("INSERT INTO 'work'(`Subject_ID`, `Year`, `WorkID`, `Deadlines`, `status`, `type`, `FullMark`, `Grading`, `lim_member`) VALUES(?,?,?,?,?,?,?,?,?);",('FRA222', '2016', 'hamID', '2016-09-07', 'Active', 'class_assignment', '20', None, 3))
+ham.execute("INSERT INTO 'work'(`Subject_ID`, `Year`, `WorkID`, `Deadlines`, `status`, `type`, `FullMark`, `Grading`, `lim_member`) VALUES(?,?,?,?,?,?,?,?,?);",('FRA222', '2016', 'hamID', '2016-09-07', 'Active', 'class_assignment', '20', None, 1))
+s.commit()
+ham.close()
+# print h.execute("SELECT * from User").fetchall()
+# print h.execute("SELECT * from User").fetchone()
 a = Data()
 
 # a.edit("DELETE FROM Enrol WHERE ID = '58340500000'")#can do all code with SQL
