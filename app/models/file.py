8b42edb41479456e353d67fc2b20375c1d8da3f9
Garsiet <garsiet1@gmail.com>
2016-11-28 08:39:33 +0700

    upload file

--- app/models/file.py
+++ app/models/file.py
@@ -1,33 +1,28 @@
-import sqlite3
-from flask import url_for
-from flask import request
-# declare class
-class file:
-    def upload_file(self):
-        if request.method == 'POST':
-            f = request.files['the_file']
-            f.save('this pc/uploads/lesson2.pdf')
+import urllib2
 
-            import urllib2
-            url = "www.pdf995.com/samples/pdf.pdf" //
-            file_name = url.split('/')[-1]
-            u = urllib2.urlopen(url)
-            f = open(file_name, 'wb')
-            meta = u.info()
-            file_size = int(meta.getheaders("Content-Length")[0])
-            print "Downloading: %s Bytes: %s" % (file_name, file_size)
-            file_size_dl = 0
-            block_sz = 8192
-            while True:
-                buffer = u.read(block_sz)
-            if not buffer:
-                break
-            file_size_dl += len(buffer)
-            f.write(buffer)
-            status = r"%10d [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8) * (len(status) + 1)
-            print status,
-            f.close()
+url = "www2.kmutt.ac.th/thai/abt_history/info.../KMUTT-Prospectus.pdf"
+
+file_name = url.split('/')[-1]
+u = urllib2.urlopen(url)
+f = open(file_name, 'wb')
+meta = u.info()
+file_size = int(meta.getheaders("Content-Length")[0])
+print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+file_size_dl = 0
+block_sz = 8192
+while True:
+    buffer = u.read(block_sz)
+    if not buffer:
+        break
+
+    file_size_dl += len(buffer)
+    f.write(buffer)
+    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+    status = status + chr(8)*(len(status)+1)
+    print status,
+
+f.close()
 
 
 
